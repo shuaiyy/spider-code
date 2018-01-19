a18c37dceca51d1eb2b9cda5508303cd46dee418
石小龙 <33952447+shixiaolong55@users.noreply.github.com>
2018-01-19 17:21:04 +0800

    调用weixin的api接口发信息
    通过导入itchat库来实现

---
+++ 群发祝福.py
@@ -0,0 +1,8 @@
+# -*- coding:UTF-8 -*-
+import itchat
+itchat.auto_login(hotReload = True)
+users = itchat.get_friends()
+for i in len(users):
+    user = itchat.get_friends()[i]
+    msc = itchat.get_friends()[i]['RemarkName'] + '老铁:圣诞节快乐哦噢噢噢噢哦'
+    user.send(msc)
