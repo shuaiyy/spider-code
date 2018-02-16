0520aa55a734e7dc11b025035ec5b8777b0eddc9
石小龙 <33952447+shixiaolong55@users.noreply.github.com>
2018-02-16 08:20:56 +0800

    修复了小BUG
    并且将备注名改成了昵称，否则有一些没有改备注名的是取不到值的

--- 群发祝福.py
+++ 群发祝福.py
@@ -2,7 +2,7 @@
 import itchat
 itchat.auto_login(hotReload = True)
 users = itchat.get_friends()
-for i in len(users):
+for i in range(len(users)):
     user = itchat.get_friends()[i]
-    msc = itchat.get_friends()[i]['RemarkName'] + '老铁:圣诞节快乐哦噢噢噢噢哦'
+    msc = itchat.get_friends()[i]['NickName'] + '老铁:圣诞节快乐哦噢噢噢噢哦'
     user.send(msc)
