f5e13275f5b66e1bf82679269a3d49be05f1a4bd
石小龙 <33952447+shixiaolong55@users.noreply.github.com>
2018-05-22 23:23:37 +0800

    Delete 群发祝福.py

--- 群发祝福.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:UTF-8 -*-
-import itchat
-itchat.auto_login(hotReload = True)
-users = itchat.get_friends()
-for i in range(len(users)):
-    user = itchat.get_friends()[i]
-    msc = itchat.get_friends()[i]['NickName'] + '老铁:圣诞节快乐哦噢噢噢噢哦'
-    user.send(msc)
